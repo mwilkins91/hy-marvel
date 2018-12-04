237095d1cf0126d65381abedce57848828e9581d
Shangni <shangnihu@gmail.com>
2018-12-04 15:06:30 -0500

    Changed label jsx

--- src/components/FilterBar.js
+++ src/components/FilterBar.js
@@ -5,10 +5,10 @@ class FilterBar extends Component {
 	render(){
 		return (<div className="FilterBar__Component">
 			<form>
-			  <label class="visually-hidden">
+			  <label class="visually-hidden" for="search">
 			    Search Characters:
 			  </label>
-			  <input type="text" placeholder="spider-man"/>
+			  <input type="text" placeholder="spider-man" id="search"/>
 			  <button type="submit">Submit</button>
 			</form>
 		</div>)
