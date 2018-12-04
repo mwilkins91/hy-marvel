4608b7fcb2b58da77ba0ff5becf3c39c43cd280d
Karley MacDonald <karleymacdonald@gmail.com>
2018-12-03 23:09:07 -0500

    Remove create react app default JSX and add initial JSX for Marvel app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <div className="App__Component">
+        <header>
+          <h1>Marvel Comic Explorer</h1>
+          <p>Search and explore your favourite Marvel characters!</p>
         </header>
+        <footer>Data provided by Marvel. © 2014 Marvel</footer>
       </div>
     );
   }
