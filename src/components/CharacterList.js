93acf04486248c21dcf158288ea50e996bc32ed6
Karley MacDonald <karleymacdonald@gmail.com>
2018-12-04 16:53:26 -0500

    Fetch character list data and render list

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import "./CharacterList.scss"; 
 
 class CharacterList extends Component {
@@ -10,8 +11,36 @@ class CharacterList extends Component {
     }
   }
 
+  componentDidMount() {
+    this.fetchCharacters();
+  }
+
+  fetchCharacters = async() => {
+    try {
+      const charactersData = await axios.get(`https://gateway.marvel.com:443/v1/public/characters`, {
+        params: {
+          apikey: "7a9e90379097ff7a2c43f9ab74f30d24",
+          limit: 50
+        }
+      });
+
+      const characters = [...this.state.characters, ...charactersData.data.data.results]
+
+      this.setState({ 
+        characters: characters
+      });
+    } catch (e) {
+      this.setState({ err: e.message })
+    } 
+  }
+
   renderCharacters() {
-    const characterList = [];
+    const characterList = this.state.characters.map((character, index) => {
+      return (<div className="character-card" key={`${character.id}-${index}`}>
+        <img alt="" src={`${character.thumbnail.path}.${character.thumbnail.extension}`}/>
+        <p>{character.name}</p>
+      </div>);
+    });
     return (<div className="character-list">{characterList}</div>);
   }
 
